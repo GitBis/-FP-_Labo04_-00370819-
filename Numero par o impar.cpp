51fa32a7a5e922914e20fb28bbd5fb6dbc227d13
GitBis <00370819@uca.edu.sv>
2019-09-30 23:55:37 -0600

    Ejercicio 3

---
+++ Numero par o impar.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int num, may;
+
+int main()
+{
+	cout << "Ingrese un numero" << endl;
+	cin >> num;
+
+	if(may = num > 0){
+	 
+		cout << "El numero ingresado es positivo" << endl;
+	}
+	else if (may = num == 0) {
+
+		cout << "El numero ingresado es cero" << endl;
+	}
+
+	else {
+	
+		cout << "El numero ingresado es negativo" << endl;
+	}
+	return 0;
+	
+}
